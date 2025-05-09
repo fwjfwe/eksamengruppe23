b906a8572629441273a4a4a2a06f02bc5d812226
DeOne313 <mbalezai@hiof.no>
2025-05-09 04:29:21 +0200

    Dashboard
    Jobba litt med dashboard, fiksa et login forum og en midlertidig css for å kunne jobbe med det. HUSK SLETT MIDLERTIDIG CSS når vi er ferdig med den.

--- my-app/src/components/Dashboard.jsx
+++ my-app/src/components/Dashboard.jsx
@@ -1,9 +1,40 @@
+import React, { useState } from "react";
+import "../styles/MIDLERTIDIGDASH.css";
 export default function Dashboard() {
-    return (
-      <div>
+  return (
+    <>
+    <section>
         <h2>Dashboard</h2>
         <p>Velkommen til ditt dashboard!</p>
-      </div>
-    )
-  }
-  
+      </section>
+
+      <section className="login-container">
+        <form className="login-form">
+          <h2>Login</h2>
+
+          <fieldset>
+            <label htmlFor="epost">Epost</label>
+            <input
+              type="epost"
+              id="epost"
+              placeholder="Skriv inn Epost"
+            />
+          </fieldset>
+
+          <fieldset>
+            <label htmlFor="passord">Passord</label>
+            <input
+              type="passord"
+              id="passord"
+              placeholder="Skriv inn passord"
+            />
+          </fieldset>
+
+          <button type="button">
+            Login
+          </button>
+        </form>
+      </section>
+    </>
+  );
+}
