f26e6bdcb90e72ed5aa2a895b87e40afef002f38
fwfjwe <isaisajev.adamovitj27@gmail.com>
2025-05-17 16:41:35 +0200

    Lagde eventpage og artistcard
    Lagde eventpage og artistcard og gjorde noen endringer i app og eventpage

--- my-app/src/components/EventCard.jsx
+++ my-app/src/components/EventCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const EventCard = ({ event }) => {
     const imageUrl = event.images?.[0]?.url;
     const name = event.name;
@@ -16,23 +18,44 @@ const EventCard = ({ event }) => {
 };
 
 export const EventGallery = ({ events }) => {
+    const uniqueEventsMap = new Map();
+
+    events.forEach(event => {
+        const attractionName = event._embedded?.attractions?.[0]?.name;
+        if (attractionName && !uniqueEventsMap.has(attractionName)) {
+            uniqueEventsMap.set(attractionName, event);
+        }
+    });
+
+    const uniqueEvents = Array.from(uniqueEventsMap.values());
+
     return (
         <>
             <h1>Sommerens festivaler!</h1>
             <section className="image-container">
-                {events.map((event) => (
-                    <article key={event.id}>
-                        {event.images?.[0]?.url && (
-                            <figure>
-                                <img className="image" src={event.images[0].url} alt={event.name} />
-                                <h4>{event.name}</h4>
-                            </figure>
-                        )}
-                    </article>
-                ))}
+                {uniqueEvents.map((event) => {
+                    const attractionId = event._embedded?.attractions?.[0]?.id; 
+
+                    return (
+                        <article key={event.id}>
+                            {event.images?.[0]?.url && (
+                                <figure>
+                                    <img className="image" src={event.images[0].url} alt={event.name} />
+                                    <h4>{event.name}</h4>
+                                    <Link to={`/event/${attractionId}`}>
+                                        <button>Les mer om {event.name}</button>
+                                    </Link>
+                                </figure>
+                            )}
+                        </article>
+                    );
+                })}
             </section>
         </>
     );
 };
+
+
+
 
 export default EventCard;
