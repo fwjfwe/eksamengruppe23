90fe3669286db67a867f542612f04766107f1f90
fwfjwe <isaisajev.adamovitj27@gmail.com>
2025-05-17 19:51:29 +0200

    satt in events i sanity

--- my-app/src/components/SanityEventDetails.jsx
+++ my-app/src/components/SanityEventDetails.jsx
@@ -2,7 +2,7 @@ export default function SanityEventDetails() {
     return (
       <div>
         <h2>Sanity Event Details</h2>
-        <p>Detaljer om eventet fra Sanity her.</p>
+        <p>Detaljer om eventet fra Sanity her.</p> //https://app.ticketmaster.com/discovery/v2/events?apikey=oUxracsATFhH1Gkva2GRyOTWWWR7uOgl&id=17GOvxG61xwC3XJ&locale=*
       </div>
     )
   }
